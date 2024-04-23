1123da2413e1ac0760aece7b4de85d2cc59c0ae3
Michael Adaixo <michael.adaixo@gmail.com>
2024-04-23 22:58:34 +0100

    new astro site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'MikeaDev';
+export const SITE_DESCRIPTION = 'Semi random thoughts and bits of code.';
