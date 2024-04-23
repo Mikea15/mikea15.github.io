13949c3fa225ec941574d7e5b9c1d7261de3d627
Michael Adaixo <michael.adaixo@gmail.com>
2024-04-23 22:52:16 +0100

    delete old

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'MikeaDev';
-export const SITE_DESCRIPTION = 'Semi random thoughts and bits of code.';
