022b89d4b675a49fdc4e175fb321aaaf3660da2e
Michael Adaixo <michael.adaixo@gmail.com>
2024-05-13 22:42:53 +0100

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -23,7 +23,7 @@ export async function GET(context: Context) {
     items: items.map((item) => ({
       title: item.data.title,
       description: item.data.description,
-      date: item.data.date,
+      pubDate: item.data.date,
       link: `/${item.collection}/${item.slug}/`,
     })),
   });
